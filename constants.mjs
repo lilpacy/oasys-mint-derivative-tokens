634f983af6b7376ee769046293e3d5d3fb1b1144
lilpacy <revivedtomorrow@gmail.com>
2023-08-02 03:00:44 +0900

    add comments

--- constants.mjs
+++ constants.mjs
@@ -1,4 +1,6 @@
+// https://github.com/oasysgames/oasys-validator/blob/72aafde1f7484fbe20589b5f67b528947ba11fa5/contracts/oasys/contracts.go#L25
 export const wOASAddress = '0x5200000000000000000000000000000000000001'
+// https://github.com/oasysgames/oasys-validator/blob/72aafde1f7484fbe20589b5f67b528947ba11fa5/contracts/oasys/contracts.go#L29
 export const sOASAddress = '0x5200000000000000000000000000000000000002'
 
 export const mainnetRpc = 'https://rpc.mainnet.oasys.games'
